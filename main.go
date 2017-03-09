bce0d21c769dcba855df8357fc32113bedde05c7
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-03-09 19:49:51 +0530

    Background now changes as a function of the hostname's sha hash

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"os"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		h, _ := os.Hostname()
-		w.Write([]byte(h))
+	h, _ := os.Hostname()
+	rdr := strings.NewReader(h)
+	hash := sha256.New()
+	io.Copy(hash, rdr)
+	color := hex.EncodeToString(hash.Sum(nil))[:6]
+
+	htmltempl := `
+		<head>
+			<title>%[2]s</title>
+			<style type="text/css">
+			body{
+				background: #%[1]s;
+				color: white;
+				font: 3vw Arial;
+				display: flex;
+				justify-content: center;
+				align-items:center;
+			}
+			</style>
+		</head>
+		<body>
+			%[2]s
+		</body>
+		`
+	html := fmt.Sprintf(htmltempl, color, h)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte(html))
 	})
-	
+
 	http.ListenAndServe(":8000", nil)
 }
